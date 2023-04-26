1ba2c8a7369353078aa406ae752393c065b29993
aagbator <aagbator@breadfinance.com>
2023-04-26 11:04:23 +0100

    added seeder

--- src/database/models/Member.ts
+++ src/database/models/Member.ts
@@ -1,9 +1,21 @@
-import { Table, Column, Model, IsEmail } from "sequelize-typescript";
+import {
+  Table,
+  Column,
+  Model,
+  IsEmail,
+  IsUUID,
+  PrimaryKey,
+} from "sequelize-typescript";
 
 type Gender = "male" | "female";
 
 @Table
 export default class Member extends Model {
+  @IsUUID(4)
+  @PrimaryKey
+  @Column
+  id!: string;
+
   @Column
   name!: string;
 }
